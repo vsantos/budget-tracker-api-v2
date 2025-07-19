3c42c55dc0d3758b8efd57523350a846cce89d17
Victor Santos <vsantos.py@gmail.com>
2025-07-19 13:24:11 -0300

    add test to middleware

--- internal/http/middleware/middleware_test.go
+++ internal/http/middleware/middleware_test.go
@@ -9,16 +9,16 @@ import (
 )
 
 func TestInjectHeaders(t *testing.T) {
-	dummyHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mockHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	wrapped := InjectHeaders(dummyHandler)
+	wrappedHandler := InjectHeaders(mockHandler)
 
 	req := httptest.NewRequest("GET", "/", nil)
 	rr := httptest.NewRecorder()
 
-	wrapped.ServeHTTP(rr, req)
+	wrappedHandler.ServeHTTP(rr, req)
 	assert.Equal(t, "application/json", rr.Header().Get("Content-Type"))
 	assert.Equal(t, "*", rr.Header().Get("Access-Control-Allow-Origin"))
 }
